2cacb2a09ae7659763ffc401d85abc97321eb748
alexisguadarrama <alexgsisc@gmail.com>
2024-11-24 20:14:58 -0600

    add provider to counter

--- lib/screens/counter/counter_screen.dart
+++ lib/screens/counter/counter_screen.dart
@@ -1,23 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:widgets_app/providers/counter_providers.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends ConsumerWidget {
   static const name = 'counter_screen';
   const CounterScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final int counter = ref.watch(counterProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Counter Screen'),
       ),
       body: Center(
         child: Text(
-          'Valor: 0',
+          'Valor: $counter',
           style: Theme.of(context).textTheme.titleLarge,
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          //ref.read(counterProvider.notifier).state++;
+          ref.read(counterProvider.notifier).update((value) => value + 1);
+        },
         child: const Icon(Icons.add),
       ),
     );
