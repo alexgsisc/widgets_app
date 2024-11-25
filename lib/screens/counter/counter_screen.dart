82a4bf445005a59697c94a908effde19051d6072
alexisguadarrama <alexgsisc@gmail.com>
2024-11-24 20:21:04 -0600

    add provider by control theme dart

--- lib/screens/counter/counter_screen.dart
+++ lib/screens/counter/counter_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_app/providers/counter_providers.dart';
+import 'package:widgets_app/providers/theme_provider.dart';
 
 class CounterScreen extends ConsumerWidget {
   static const name = 'counter_screen';
@@ -9,10 +10,23 @@ class CounterScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final int counter = ref.watch(counterProvider);
+    final bool isDark = ref.watch(isDarkProvider);
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Counter Screen'),
+        title: const Text(
+          'Counter Screen',
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              ref.read(isDarkProvider.notifier).update((value) => !value);
+            },
+            icon: Icon(
+              isDark ? Icons.dark_mode_outlined : Icons.light_mode_outlined,
+            ),
+          )
+        ],
       ),
       body: Center(
         child: Text(
